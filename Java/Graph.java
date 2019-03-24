00dcc86d4a8752833f1712753c1d14bbe5dc6d2a
Allen Philip <allenphilip93@gmail.com>
2019-03-25 00:23:59 +0530

    Graph and Strings

--- Java/Graph.java
+++ Java/Graph.java
@@ -15,28 +15,37 @@ import java.util.Set;
 public class Graph<V> {
 
     private Map<V, List<V>> adjacencyMap = new HashMap<>();
+    private Map<V, Map<V, Integer>> weightMap = new HashMap<>();
+    private Map<V, V> parent = new HashMap<>();
+    private Map<V, Integer> level = new HashMap<>();
 
     public void addVertex(V vertex) {
         if (!adjacencyMap.containsKey(vertex)) {
             adjacencyMap.put(vertex, new ArrayList<>());
+            weightMap.put(vertex, new HashMap<>());
             // System.out.println("Added vertext : " + vertex);
         } else {
             System.out.println("Vertex " + vertex + " already exists!");
         }
     }
 
-    public void addEdge(V source, V dest) {
+    public void addEdge(V source, V dest, int weight) {
         if (!adjacencyMap.containsKey(source)) {
             System.out.println("Vertex " + source + " is not found!");
         } else if (!adjacencyMap.containsKey(dest)) {
             System.out.println("Vertex " + dest + " is not found!");
         } else {
             adjacencyMap.get(source).add(dest);
+            weightMap.get(source).put(dest, weight);
             // System.out.println("Edge from vertex(" + source + ") to vertex (" + dest + ") has been added!");
         }
     }
 
-    public void bfs(V source) {
+    public void addEdge(V source, V dest) {
+        addEdge(source, dest, 1);
+    }
+
+    public void bfsBasic(V source) {
         Deque<V> queue = new ArrayDeque<>();
         Set<V> visited = new HashSet<>();
         queue.offer(source);
@@ -56,19 +65,130 @@ public class Graph<V> {
         System.out.println("BFS Traversal : " + s);
     }
 
-    public void dfs(V source) {
-        System.out.println("DFS Traversal : START => " + dfs(source, new HashSet<V>()) + "END");
+    public void bfs(V source) {
+      String s = "START => " + source + " => ";
+      Map<V, V> parent = new HashMap<>();
+      parent.put(source, null);
+      Map<V, Integer> level = new HashMap<>();
+      int currLevel = 0;
+      level.put(source, currLevel);
+      Set<V> frontier = new HashSet<>();
+      Set<V> next;
+      frontier.add(source);
+      currLevel++;
+      while (frontier.size() > 0) {
+        next = new HashSet<>();
+        for (V vertex : frontier) {
+          for (V childVertex : adjacencyMap.get(vertex)) {
+            if (!level.containsKey(childVertex)) {
+              parent.put(childVertex, vertex);
+              level.put(childVertex, currLevel);
+              s = s + childVertex + " => ";
+              next.add(childVertex);
+            }
+          }
+        }
+        frontier = next;
+        currLevel++;
+      }
+      s = s + "END";
+      System.out.println("BFS Traversal : " + s);
     }
 
-    private String dfs(V source, Set<V> visited) {
+    public void dfs() {
+        parent = new HashMap<>();
+        String s = "START => ";
+        for (V source : adjacencyMap.keySet()) {
+            if (!parent.containsKey(source)) {
+                parent.put(source, null);
+                s = s + dfs(source, parent);
+            }
+        }
+        s = s + "END";
+        System.out.println("DFS Traversal : " + s);
+    }
+
+    public void dfs(V source) {
+        parent.put(source, null);
+        System.out.println("DFS Traversal : START => " + dfs(source, parent) + "END");
+    }
+
+    private String dfs(V source, Map<V, V> parent) {
         String s = "";
         s = s + source + " => ";
-        visited.add(source);
         for (V vertex : adjacencyMap.get(source)) {
-            if (!visited.contains(vertex))
-                s = s + dfs(vertex, visited);
+            if (!parent.containsKey(vertex)) {
+                parent.put(vertex, source);
+                s = s + dfs(vertex, parent);
+            }
         }
         return s;
+    }
+
+    int time = 0;
+    public void classifyEdges() {
+        parent = new HashMap<>();
+        Map<V, Integer> timeMap = new HashMap<>(); // Stores the time at which vertex v was last visited
+        Set<V> connectedSet = new HashSet<>();
+        time = 0;
+        for (V source : adjacencyMap.keySet()) {
+            if (!parent.containsKey(source)) {
+                parent.put(source, null);
+                dfsEdgeClassify(source, timeMap, connectedSet);
+                connectedSet.addAll(parent.keySet());
+            }
+        }
+    }
+
+    private void dfsEdgeClassify(V source, Map<V,Integer> timeMap, Set<V> preConnectedSet) {
+        time = time + 1;
+        timeMap.put(source, time);
+        for (V child : adjacencyMap.get(source)) {
+            if (parent.containsKey(child)) {
+                if (preConnectedSet.contains(child)) {
+                    System.out.println("Edge (" + source + ", " + child + ") is a cross edge!");
+                } else if (timeMap.get(child) <= timeMap.get(source)) {
+                    System.out.println("Edge (" + source + ", " + child + ") is a backward edge!");
+                } else {
+                    System.out.println("Edge (" + source + ", " + child + ") is a forward edge!");
+                }
+            } else {
+                System.out.println("Edge (" + source + ", " + child + ") is a tree edge!");
+                parent.put(child, source);
+                dfsEdgeClassify(child, timeMap, preConnectedSet);
+            }
+        }
+        time = time + 1;
+        timeMap.put(source, time);
+    }
+
+    // Graph needs to be a DAG - Directed Acyclic Graph
+    public void topologicalSort() {
+        dfs();
+        parent = new HashMap<>();
+        List<V> priority = new ArrayList<>();
+        for (V source : adjacencyMap.keySet()) {
+            if (!parent.containsKey(source)) {
+                parent.put(source, null);
+                topologicalSort(source, priority);
+            }
+        }
+        String s = "START => ";
+        for (int index = priority.size()-1; index >= 0; index--) {
+            s = s + priority.get(index) + " => ";
+        }
+        s = s + "END";
+        System.out.println("Topological Sorted Flow : " + s);
+    }
+
+    private void topologicalSort(V source, List<V> priority) {
+        for (V child : adjacencyMap.get(source)) {
+            if (!parent.containsKey(child)) {
+                parent.put(child, source);
+                topologicalSort(child, priority);
+            }
+        }
+        priority.add(source);
     }
 
     public void print() {
@@ -76,6 +196,36 @@ public class Graph<V> {
         System.out.println("===============");
         for (V vertex : adjacencyMap.keySet()) {
             System.out.println(vertex + " : " + Arrays.toString(adjacencyMap.get(vertex).toArray()));
+        }
+    }
+
+    public void shortestPath(V source) {
+        Map<V, Integer> minPath = new HashMap<>();
+        Map<V, V> pred = new HashMap<>();
+        minPath.put(source, 0);
+        pred.put(source, source);
+        Deque<V> queue = new ArrayDeque<>();
+        queue.offer(source);
+        Set<V> visited = new HashSet<>();
+        while (queue.size() > 0) {
+            V vertex = queue.remove();
+            for (V child : adjacencyMap.get(vertex)) {
+                if (visited.contains(child)) {
+                    if (minPath.get(child).compareTo(weightMap.get(vertex).get(child) + minPath.get(vertex)) > 0) {
+                        minPath.put(child, weightMap.get(vertex).get(child) + minPath.get(vertex));
+                        pred.put(child, vertex);
+                    }
+                    queue.offer(child);
+                } else {
+                    minPath.put(child, weightMap.get(vertex).get(child) + minPath.get(vertex));
+                    pred.put(child, source);
+                    queue.offer(child);
+                }
+            }
+            visited.add(vertex);
+        }
+        for (V vertex : minPath.keySet()) {
+            System.out.println("Shortest Path Dist from " + source + " to " + vertex + " : " + minPath.get(vertex));
         }
     }
 
@@ -87,18 +237,46 @@ public class Graph<V> {
         graph.addVertex("C");
         graph.addVertex("D");
         graph.addVertex("E");
-        graph.addEdge("A", "C");
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "A");
-        graph.addEdge("C", "A");
-        graph.addEdge("C", "D");
-        graph.addEdge("D", "C");
-        graph.addEdge("B", "E");
-        graph.addEdge("E", "B");
-        graph.addEdge("D", "E");
-        graph.addEdge("E", "D");
-        graph.print();
-        graph.bfs("E");
-        graph.dfs("E");
+        // graph.addVertex("F");
+        graph.addEdge("A", "B", 4);
+        graph.addEdge("C", "B", 2);
+        graph.addEdge("A", "C", 1);
+        graph.addEdge("C", "D", 4);
+        graph.addEdge("B", "E", 4);
+        graph.addEdge("D", "E", 4);
+        // graph.addEdge("A", "B");
+        // graph.addEdge("A", "D");
+        // graph.addEdge("B", "E");
+        // graph.addEdge("E", "D");
+        // graph.addEdge("D", "B");
+        // graph.addEdge("C", "E");
+        // graph.addEdge("C", "F");
+        // graph.addEdge("F", "F");
+        // graph.print();
+        // graph.addVertex("undershorts");
+        // graph.addVertex("pants");
+        // graph.addVertex("belt");
+        // graph.addVertex("shirt");
+        // graph.addVertex("tie");
+        // graph.addVertex("jacket");
+        // graph.addVertex("watch");
+        // graph.addVertex("socks");
+        // graph.addVertex("shoes");
+        // graph.addEdge("undershorts", "pants");
+        // graph.addEdge("undershorts", "socks");
+        // graph.addEdge("pants", "shoes");
+        // graph.addEdge("pants", "belt");
+        // graph.addEdge("belt", "jacket");
+        // graph.addEdge("shirt", "tie");
+        // graph.addEdge("tie", "jacket");
+        // graph.addEdge("shirt", "belt");
+        // graph.addEdge("socks", "shoes");
+
+        // graph.bfs("A");
+        // graph.dfs("A");
+        // graph.classifyEdges();
+        // graph.dfs();
+        // graph.topologicalSort();
+        graph.shortestPath("A");
     }
 }
--- Java/StringAlgorithms.java
+++ Java/StringAlgorithms.java
@@ -1,3 +1,10 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
 public class StringAlgorithms {
 
   public static String reverse(String str) {
@@ -118,37 +125,268 @@ public class StringAlgorithms {
     return (str.charAt(index) == str.charAt(leftIdx) || str.charAt(index) == str.charAt(rightIdx));
   }
 
-  public void minContainingWindow(String text, String pattern) {
-
+  public static void minContainingWindow(String text, String pattern) {
+      Map<Character, Integer> windowMap = new HashMap<>();
+      Map<Character, Integer> patternMap = new HashMap<>();
+      // Update the required patternMap
+      for (int index=0; index < pattern.length(); index++) {
+          Character c = pattern.charAt(index);
+          if (!patternMap.containsKey(c)) {
+              patternMap.put(c, 1);
+          } else {
+              patternMap.put(c, patternMap.get(c)+1);
+          }
+      }
+      // Update the window map assuming max window size of length of text
+      for (int index=0; index < text.length(); index++) {
+          Character c = text.charAt(index);
+          if (!windowMap.containsKey(c)) {
+              windowMap.put(c, 1);
+          } else {
+              windowMap.put(c, windowMap.get(c)+1);
+          }
+      }
+      // Shrink from front
+      int front=0;
+      for (; front < text.length(); front++) {
+          Character c = text.charAt(front);
+          if (patternMap.containsKey(c) && windowMap.get(c) == patternMap.get(c)) {
+              break;
+          }
+          if (patternMap.containsKey(c)) {
+              windowMap.put(c, windowMap.get(c)-1);
+          }
+      }
+      // Shrink from back
+      int rear=text.length()-1;
+      for (; rear > front; rear--) {
+          Character c = text.charAt(rear);
+          if (patternMap.containsKey(c) && windowMap.get(c) == patternMap.get(c)) {
+              break;
+          }
+          if (patternMap.containsKey(c)) {
+              windowMap.put(c, windowMap.get(c)-1);
+          }
+      }
+      System.out.println("Min Enclosing Window Size : " + (rear - front + 1));
+      System.out.println("Min Enclosing Window : " + text.substring(front, rear + 1));
   }
 
-  public void interleave(String str1, String str2) {
-
+  // Assume that its a n x n input array
+  public static void towDimWordSearch(char[][] array, String pattern) {
+    Map<Character, Integer> patternMap = new HashMap<>();
+    // Update the required patternMap
+    for (int index=0; index < pattern.length(); index++) {
+        Character c = pattern.charAt(index);
+        if (!patternMap.containsKey(c)) {
+            patternMap.put(c, 1);
+        } else {
+            patternMap.put(c, patternMap.get(c)+1);
+        }
+    }
+    // find a valid starting point
+    // for each match we will call the subsequent match finding block until we find a match
+    int x=0, y=0;
+    boolean found = false;
+    int[][] history = new int[array.length][array.length];
+    for (; x < array.length; x++) {
+        for (y = x; y < array.length; y++) {
+            history[x][y] = 1;
+            if (patternMap.containsKey(array[x][y])) {
+                Map<Character, Integer> patternCopy = new HashMap<Character, Integer>();
+                // TODO - similarly need to clone and send history too
+                patternCopy.putAll(patternMap);
+                found = findTwoDimMatch(x, y, array.length, history, patternCopy, array);
+                if (found)
+                    break;
+            }
+        }
+        if (found)
+            break;
+    }
   }
 
-  public void rowColMatch(int[][] array) {
-
+  private static boolean findTwoDimMatch(int x, int y, int length, int[][] history, Map<Character, Integer> patternMap, char[][] array) {
+    Deque<List<Integer>> queue = new ArrayDeque<>();
+    List<Integer> tuple = new ArrayList<>();
+    tuple.add(x); tuple.add(y);
+    queue.offer(tuple);
+    while (queue.size() > 0) {
+        List<Integer> pair = queue.remove();
+        x = pair.get(0);
+        y = pair.get(1);
+        history[x][y] = 1;
+        if (patternMap.containsKey(array[x][y])) {
+            System.out.println("X : " + x + " Y : " + y + " CHAR : " + array[x][y]);
+            int count = patternMap.get(array[x][y]) - 1;
+            if (count == 0) {
+                patternMap.remove(array[x][y]);
+            } else {
+                patternMap.put(array[x][y], count);
+            }
+        }
+        // check for match
+        if (patternMap.size() == 0) {
+            break;
+        }
+        // offer all neighbours of current pos
+        offerAllImmediatePos(x, y, array.length, queue, history, patternMap, array);
+    }
+    if (patternMap.size() > 0){
+        System.out.println("Match not found!");
+    } else {
+        System.out.println("Match Found!");
+    }
+    return patternMap.size() == 0;
   }
 
-  public void replace(String str, String find, String replace) {
-
+  private static void offerAllImmediatePos(int x, int y, int length, Deque<List<Integer>> queue, int[][] history, Map<Character, Integer> patternMap, char[][] array) {
+    if (x-1 >= 0 && y-1 >= 0 && history[x-1][y-1] == 0 && patternMap.containsKey(array[x-1][y-1])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x-1);
+        tuple.add(y-1);
+        queue.offer(tuple);
+    }
+    if (x-1 >= 0 && history[x-1][y] == 0 && patternMap.containsKey(array[x-1][y])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x-1);
+        tuple.add(y);
+        queue.offer(tuple);
+    }
+    if (y-1 >= 0 && history[x][y-1] == 0 && patternMap.containsKey(array[x][y-1])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x);
+        tuple.add(y-1);
+        queue.offer(tuple);
+    }
+    if (x-1 >= 0 && y+1 < length && history[x-1][y+1] == 0 && patternMap.containsKey(array[x-1][y+1])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x-1);
+        tuple.add(y+1);
+        queue.offer(tuple);
+    }
+    if (y+1 < length && history[x][y+1] == 0 && patternMap.containsKey(array[x][y+1])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x);
+        tuple.add(y+1);
+        queue.offer(tuple);
+    }
+    if (x+1 < length && y+1 < length && history[x+1][y+1] == 0 && patternMap.containsKey(array[x+1][y+1])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x+1);
+        tuple.add(y+1);
+        queue.offer(tuple);
+    }
+    if (x+1 < length && history[x+1][y] == 0 && patternMap.containsKey(array[x+1][y])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x+1);
+        tuple.add(y);
+        queue.offer(tuple);
+    }
+    if (x+1 < length && y-1 >= 0 && history[x+1][y-1] == 0 && patternMap.containsKey(array[x+1][y-1])) {
+        List<Integer> tuple = new ArrayList<>();
+        tuple.add(x+1);
+        tuple.add(y-1);
+        queue.offer(tuple);
+    }
   }
 
-  public void encodeString(String str) {
-
+  public static void interleave(String str1, String str2) {
+    interleave(str1, str2, "");
   }
 
-  public void captureXOs(Character[][] c) {
-    
+  private static void interleave(String str1, String str2, String res) {
+    if (str1.length() == 0 && str2.length() > 0) {
+        res = res + str2;
+        System.out.println("Interleaved String : " + res);
+    } else if (str1.length() > 0 && str2.length() == 0) {
+        res = res + str1;
+        System.out.println("Interleaved String : " + res);
+    } else if (str1.length() > 0 && str2.length() > 0) {
+        int startIndex = 0;
+        int endIndex = str1.length();
+            for (int index=startIndex; index <= endIndex; index++) {
+                interleave(
+                    getSubString(str1, index, str1.length()), 
+                    getSubString(str2, 1, str2.length()),
+                    res + getSubString(str1, 0, index) + str2.charAt(0)
+                );
+            }
+    } else {
+        System.out.println("Interleaved String : " + res);
+    }
+  }
+
+  public static void rowColMatch(int[][] array) {
+    // use tries of rows and compare the columns O(R+C)
+  }
+
+  public static void replace(String str, String find, String replace) {
+    // Start doing from the end so that the no of overwrites is minimal
+  }
+
+  public static void encodeString(String str) {
+    // Identify if encoding is necessary in O(n)
+    char prev = str.charAt(0), curr = str.charAt(0);
+    int count = 0;
+    for (int index=1; index < str.length(); index++) {
+        curr = str.charAt(index);
+        if (curr == prev) {
+            count++;
+        } else {
+            prev = curr;
+        }
+    }
+    if ((str.length() - count) <= count) {
+        // Encoding is useful
+        StringBuilder sb = new StringBuilder();
+        prev = str.charAt(0);
+        curr = str.charAt(0);
+        count = 1;
+        for (int index=1; index < str.length(); index++) {
+            curr = str.charAt(index);
+            if (curr == prev) {
+                count++;
+            } else {
+                sb.append(prev);
+                sb.append(count);
+                prev = curr;
+                count = 1;
+            }
+        }
+        if (prev == curr) {
+            sb.append(curr);
+            sb.append(count);
+        }
+        System.out.println("Encoded String : " + sb.toString());
+    } else {
+        // Encoding is not useful
+        System.out.println("Unencoded String : " + str);
+    }
+  }
+
+  public static void captureXOs(Character[][] c) {
+
   }
 
 
   public static void main(String[] args) {
-    StringAlgorithms.reverse("dracula");
-    StringAlgorithms.reverseWords("mridula sounds lot like dracula");
-    StringAlgorithms.allPermutations("cat");
-    StringAlgorithms.allCombinations("abc");
-    StringAlgorithms.recursivelyRemoveDuplicates("mississippi");
+    // StringAlgorithms.reverse("dracula");
+    // StringAlgorithms.reverseWords("mridula sounds lot like dracula");
+    // StringAlgorithms.allPermutations("cat");
+    // StringAlgorithms.allCombinations("abc");
+    // StringAlgorithms.recursivelyRemoveDuplicates("mississippi");
     // StringAlgorithms.recursivelyRemoveDuplicates("allen");
+    StringAlgorithms.minContainingWindow("ABBACBAA", "AAB");
+    // StringAlgorithms.towDimWordSearch(
+    //     new char[][]{
+    //         {'A', 'C', 'P', 'R', 'C'},
+    //         {'X', 'S', 'O', 'P', 'C'},
+    //         {'V', 'O', 'V', 'N', 'I'},
+    //         {'W', 'G', 'F', 'M', 'N'},
+    //         {'T', 'A', 'Q', 'I', 'Y'}
+    //     }, "MICROSOFT");
+    // StringAlgorithms.interleave("ABC", "DEF");
+    StringAlgorithms.encodeString("PHILIP");
   }
 }
